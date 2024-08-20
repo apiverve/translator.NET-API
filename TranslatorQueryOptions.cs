a4bfe9529bdaea3094e5323bea214b56b4d6a24e
APIVerve Team <hello@apiverve.com>
2024-08-20 11:12:12 -0500

    Bug Fixes

---
+++ TranslatorQueryOptions.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class TranslatorQueryOptions {
+public string text { get; set; }
+public string target { get; set; }
+public string source { get; set; }
+}
+
+}
